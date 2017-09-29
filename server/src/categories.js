e559854ff0b7c94fd1ec9b95284b63cae4044c3c
Sara <sara.her.su@gmail.com>
2017-09-29 12:24:43 +0100

    refactor: Default data for server in json files
    The default data for each of the different API components is now included
    in json files instead of being hard-coded in the code.

--- server/src/categories.js
+++ server/src/categories.js
@@ -3,22 +3,7 @@ const config = require('./config')
 
 let db = {}
 
-const defaultData = {
-  categories: [
-      {
-        name: 'react',
-        path: 'react'
-      },
-      {
-        name: 'redux',
-        path: 'redux'
-      },
-      {
-        name: 'udacity',
-        path: 'udacity'
-      }
-  ]
-}
+const defaultData = require('../data/categories.json');
 
 function getData (token) {
   //Each token has it's own copy of the DB. The token in this case is like an app id.
--- server/src/posts.js
+++ server/src/posts.js
@@ -2,28 +2,7 @@ const clone = require('clone')
 
 let db = {}
 
-const defaultData = {
-  "8xf0y6ziyjabvozdd253nd": {
-    id: '8xf0y6ziyjabvozdd253nd',
-    timestamp: 1467166872634,
-    title: 'Udacity is the best place to learn React',
-    body: 'Everyone says so after all.',
-    author: 'thingtwo',
-    category: 'react',
-    voteScore: 6,
-    deleted: false
-  },
-  "6ni6ok3ym7mf1p33lnez": {
-    id: '6ni6ok3ym7mf1p33lnez',
-    timestamp: 1468479767190,
-    title: 'Learn Redux in 10 minutes!',
-    body: 'Just kidding. It takes more than 10 minutes to learn technology.',
-    author: 'thingone',
-    category: 'redux',
-    voteScore: -5,
-    deleted: false
-  }
-}
+const defaultData = require('../data/posts.json');
 
 function getData (token) {
   let data = db[token]
