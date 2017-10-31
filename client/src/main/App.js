54055c721c209b5e296de5699214d59d5f067425
Sara <sara.her.su@gmail.com>
2017-10-31 14:00:46 +0000

    feat: Display list of posts
    The 'App' component does now call the Posts API to get a list of all the
    posts available in the server. Their information is now displayed on the
    screen (even though no design is yet being applied, everything's mark-up
    only to prioritize base functionality).

--- client/src/main/App.js
+++ client/src/main/App.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
 
+import * as PostsAPI from './utils/PostsAPI';
+
 class App extends Component {
+  state = {
+    posts: []
+  };
+
+  componentWillMount () {
+    PostsAPI.getAll()
+      .then((posts) => {
+        console.log(`${posts.length} posts found`);
+        this.setState({ posts });
+      });
+  }
+
   render() {
     return (
-      <div>Empty app</div>
+      <div>
+        <h1>Posts</h1>
+        {this.state.posts.map((post) => {
+          let score = post.voteScore;
+          return (
+            <article key={post.id}>
+              <h2>{post.title}</h2>
+              <p>Posted by {post.author} on {new Date(post.timestamp).toLocaleString()} · {post.commentCount} comments</p>
+              <p>{post.body}</p>
+              <div className={'score ' + (score > 0 ? 'positive' : (score < 0 ? 'negative' : null))}>{score}</div>
+            </article>
+          );
+        })}
+      </div>
     );
   }
 }
