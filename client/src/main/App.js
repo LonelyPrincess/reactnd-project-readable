2228349cd5d41ff6bb254b09aea053c170385cbc
Sara <sara.her.su@gmail.com>
2017-11-09 15:26:50 +0000

    design: Add some initial styles to main page
    Added some new css code to make the app have some sort of styling.
    
    The css file needs some serious clean up and it's yet far from being
    complete, but it's acceptable enough to get started.

--- client/src/main/App.js
+++ client/src/main/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import * as PostActions from './actions/post';
+import * as CommentActions from './actions/comment';
 import * as CategoryActions from './actions/category';
 
 class App extends Component {
@@ -23,34 +24,66 @@ class App extends Component {
     this.props.actions.filterPostsBy({ category });
   };
 
+  getCommentsFor = (post) => {
+    this.props.actions.fetchCommentsForPost({ post });
+  };
+
   render() {
     return (
       <div>
-        <h1>Categories</h1>
-        <ul>
-          {this.props.categories.map((category) => (
-            <li key={category.path} onClick={() => this.filterPostsBy(category)}>{category.name}</li>
-          ))}
-        </ul>
-        <h1>Posts</h1>
-        <button onClick={() => this.sortPostsBy('timestamp')}>Sort by date</button>
-        <button onClick={() => this.sortPostsBy('voteScore')}>Sort by score</button>
-        {this.props.posts.map((post) => {
-          let score = post.voteScore;
-          return (
-            <article key={post.id}>
-              <h2>{post.title}</h2>
-              <p>Posted by {post.author} on {new Date(post.timestamp).toLocaleString()} · {post.commentCount} comments</p>
-              <p>{post.body}</p>
-              <div className={'score ' + (score > 0 ? 'positive' : (score < 0 ? 'negative' : null))}>{score}</div>
-              <div className="actions">
-                <button onClick={() => this.incrementPostScore(post, "upVote")}>Upvote</button>
-                <button onClick={() => this.incrementPostScore(post, "downVote")}>Downvote</button>
-                <button onClick={() => this.props.actions.deletePost({ post })}>Delete</button>
-              </div>
-            </article>
-          );
-        })}
+        <div className="container logo">
+          <header>
+            <h1>★ Readable ★</h1>
+            <small><em>Read</em> to learn and <em>post</em> your own vision of the world</small>
+          </header>
+        </div>
+
+        <div className="container">
+          <aside>
+            <section className="side-block">
+              <h1>Categories</h1>
+              <ul>
+                {this.props.categories.map((category) => (
+                  <li key={category.path} onClick={() => this.filterPostsBy(category)}>{category.name}</li>
+                ))}
+              </ul>
+            </section>
+          </aside>
+
+          <main>
+            <h1>Posts</h1>
+            <button onClick={() => this.sortPostsBy('timestamp')}>Sort by date</button>
+            <button onClick={() => this.sortPostsBy('voteScore')}>Sort by score</button>
+            {this.props.posts.map((post) => {
+              let score = post.voteScore;
+              return (
+                <article className="post" key={post.id}>
+                  <h2>{post.title}</h2>
+                  <small>Posted by <em>{post.author}</em> on {new Date(post.timestamp).toLocaleString()} · {post.commentCount} comments</small>
+                  <main className="post-body">{post.body}</main>
+                  <div className={'score ' + (score > 0 ? 'positive' : (score < 0 ? 'negative' : null))}>{score}</div>
+                  <div className="actions">
+                    <button onClick={() => this.incrementPostScore(post, "upVote")}>Upvote</button>
+                    <button onClick={() => this.incrementPostScore(post, "downVote")}>Downvote</button>
+                    <button onClick={() => this.props.actions.deletePost({ post })}>Delete</button>
+                    <button onClick={() => this.getCommentsFor(post)}>Get comments</button>
+                  </div>
+                </article>
+              );
+            })}
+
+            <h3>Comments</h3>
+            <div className="comment-box">
+              {this.props.comments.map((comment) => (
+                <div className="comment" key={comment.id}>
+                  {comment.body}
+                  <small>Posted by <em>{comment.author}</em> on {new Date(comment.timestamp).toLocaleString()}</small>
+                </div>
+              ))}
+            </div>
+          </main>
+        </div>
+
       </div>
     );
   }
@@ -61,7 +94,8 @@ class App extends Component {
 function mapStateToProps(state) {
   return {
     posts: state.postReducer,
-    categories: state.categoryReducer
+    categories: state.categoryReducer,
+    comments: state.commentReducer
   };
 }
 
@@ -73,7 +107,8 @@ function mapDispatchToProps(dispatch) {
       updatePostScore: (data) => dispatch(PostActions.updatePostScore(data)),
       sortPostsBy: (data) => dispatch(PostActions.sortPostsBy(data)),
       filterPostsBy: (data) => dispatch(PostActions.filterPostsBy(data)),
-      fetchCategories: () => dispatch(CategoryActions.fetchCategories())
+      fetchCategories: () => dispatch(CategoryActions.fetchCategories()),
+      fetchCommentsForPost: (data) => dispatch(CommentActions.fetchCommentsForPost(data))
     }
   };
 }
