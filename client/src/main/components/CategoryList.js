5c2b9849d24fbc01c102038c15ea731f7cdced53
Sara <sara.her.su@gmail.com>
2017-11-13 19:11:49 +0000

    refactor: Component for category list

---
+++ client/src/main/components/CategoryList.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import * as CategoryActions from '../actions/category';
+
+/**
+ * Component with the list of available categories.
+ *
+ * @module components/CategoryList
+ * @author Sara Hernández <sara.her.su@gmail.com>
+ */
+class CategoryList extends Component {
+
+  componentWillMount () {
+    this.props.actions.fetchCategories();
+  }
+
+  render () {
+    return (
+      <ul>
+        <li><Link to="/">All</Link></li>
+        {this.props.categories.map((category) => (
+          <li key={category.path}>
+            <Link to={`/category/${category.path}`}>{category.name}</Link>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+};
+
+function mapStateToProps(state) {
+  return {
+    categories: state.categoryReducer
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      fetchCategories: () => dispatch(CategoryActions.fetchCategories())
+    }
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryList);
