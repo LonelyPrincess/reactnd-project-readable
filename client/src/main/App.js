a9b672809cd01e14827c9b739fa6d9939077ade0
Sara <sara.her.su@gmail.com>
2017-11-13 19:27:47 +0000

    feat + refactor: Routes for main, category and details
    Some of the content in the 'App' component has been delegated into more
    specific components, although there's yet work to be done.
    
    While refactoring this code, I've added React Router components into the
    mix. There's now three different routes available in the app:
    
     - The main page, which will display all of the available posts.
    
     - The category view, which will only contain the list of the post tagged
       under the specified category.
    
     - The post details view, where the user will be able to read all of the
       post information along with its comments.
    
    While working on this commit, many issues arised from using React Router
    along with Redux. Wrapping the 'connect' method with 'withRouter' made it
    possible to navigate from one page to another, but I still had issues with
    routes with parameters.
    
    If I navigated from a category page to another, the url would be updated
    in the browser but the render method wouldn't be called.
    
    Adding a key to the rendered component appeared to somehow solve the issue, but it's very
    likely we'll come accross this problem again in the future. Finding a real
    solution instead of forcing the component to re-render with this patch
    would be good, so we may take note of this for upcoming updates.

--- client/src/main/App.js
+++ client/src/main/App.js
@@ -1,29 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Route, withRouter } from 'react-router-dom';
 
 import * as PostActions from './actions/post';
-import * as CommentActions from './actions/comment';
-import * as CategoryActions from './actions/category';
 
-import PostListItem from './components/PostListItem';
+import PostList from './components/PostList';
+import PostDetails from './components/PostDetails';
+import CategoryList from './components/CategoryList';
 
 class App extends Component {
-  componentWillMount () {
-    this.props.actions.fetchPosts();
-    this.props.actions.fetchCategories();
-  }
 
   sortPostsBy = (criteria) => {
     this.props.actions.sortPostsBy({ criteria });
-  };
-
-  filterPostsBy = (category) => {
-    console.log(`Filtering posts by category ${category.name}...`);
-    this.props.actions.filterPostsBy({ category });
-  };
-
-  getCommentsFor = (post) => {
-    this.props.actions.fetchCommentsForPost({ post });
   };
 
   render() {
@@ -38,11 +26,7 @@ class App extends Component {
           <aside>
             <section className="side-block">
               <h1>Categories</h1>
-              <ul>
-                {this.props.categories.map((category) => (
-                  <li key={category.path}><a onClick={() => this.filterPostsBy(category)}>{category.name}</a></li>
-                ))}
-              </ul>
+              <CategoryList />
             </section>
 
             <section className="side-block">
@@ -56,28 +40,24 @@ class App extends Component {
             <section className="side-block">
               <h1>Readable</h1>
               <ul>
-                <li><a href="https://es.linkedin.com/in/sara-hern%C3%A1ndez-su%C3%A1rez-167013115" target="_blank">Author</a></li>
-                <li><a href="https://github.com/LonelyPrincess/reactnd-project-readable" target="_blank">Repository</a></li>
+                <li><a href="https://es.linkedin.com/in/sara-hern%C3%A1ndez-su%C3%A1rez-167013115" target="_blank" rel="noopener noreferrer">Author</a></li>
+                <li><a href="https://github.com/LonelyPrincess/reactnd-project-readable" target="_blank" rel="noopener noreferrer">Repository</a></li>
               </ul>
             </section>
           </aside>
 
           <main>
-            <div className="roadmap"><a>Main</a> > <a>List</a></div>
+            <Route path="/category/:category" render={({ match }) => (
+              <PostList
+                key={match.params.category}
+                category={match.params.category} />
+            )} />
 
-            {this.props.posts.map((post) => (
-              <PostListItem key={post.id} post={post} />
-            ))}
+            <Route path="/" exact component={PostList} />
 
-            <h3>Comments</h3>
-            <div className="comment-box">
-              {this.props.comments.map((comment) => (
-                <div className="comment" key={comment.id}>
-                  {comment.body}
-                  <small>Posted by <em>{comment.author}</em> on {new Date(comment.timestamp).toLocaleString()}</small>
-                </div>
-              ))}
-            </div>
+            <Route path="/post/:postId" render={({ match }) => (
+              <PostDetails postId={match.params.postId} />
+            )} />
           </main>
         </div>
 
@@ -86,26 +66,12 @@ class App extends Component {
   }
 }
 
-// We transform the state structure as we had it in our reducer
-// as we want it in our component props
-function mapStateToProps(state) {
-  return {
-    posts: state.postReducer,
-    categories: state.categoryReducer,
-    comments: state.commentReducer
-  };
-}
-
 function mapDispatchToProps(dispatch) {
   return {
     actions: {
-      fetchPosts: () => dispatch(PostActions.fetchPosts()),
-      sortPostsBy: (data) => dispatch(PostActions.sortPostsBy(data)),
-      filterPostsBy: (data) => dispatch(PostActions.filterPostsBy(data)),
-      fetchCategories: () => dispatch(CategoryActions.fetchCategories()),
-      fetchCommentsForPost: (data) => dispatch(CommentActions.fetchCommentsForPost(data))
+      sortPostsBy: (data) => dispatch(PostActions.sortPostsBy(data))
     }
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default withRouter(connect(null, mapDispatchToProps)(App));
