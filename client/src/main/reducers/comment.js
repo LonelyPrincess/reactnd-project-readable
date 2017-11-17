0f16a7d90ecb7103551e4325c8baff9c62e6edf8
Sara <sara.her.su@gmail.com>
2017-11-17 11:50:02 +0000

    feat: Sort comments after update, fetch or creation

--- client/src/main/reducers/comment.js
+++ client/src/main/reducers/comment.js
@@ -18,18 +18,11 @@ function comments (state = initialState, action) {
   switch (action.type) {
     case FETCH_COMMENTS_FOR_POST:
       updatedState = action.status === 'success' ? action.response: [];
-      updatedState.sort((a, b) => {
-        if (a['voteScore'] > b['voteScore']) {
-          return -1;
-        } else if (a['voteScore'] < b['voteScore']) {
-          return 1;
-        } else {
-          return 0;
-        }
-      });
+      updatedState.sort(compareScore);
       break;
     case POST_NEW_COMMENT:
       updatedState.push(action.response);
+      updatedState.sort(compareScore);
       break;
     case UPDATE_COMMENT_SCORE:
       console.log(`Updating score for comment ${action.response.id}`);
@@ -38,6 +31,7 @@ function comments (state = initialState, action) {
         ...updatedState[commentIndex],
         voteScore: action.response.voteScore
       };
+      updatedState.sort(compareScore);
       break;
     case DELETE_COMMENT:
       console.log(`Comment ${action.response.id} removed`);
@@ -49,6 +43,16 @@ function comments (state = initialState, action) {
   }
 
   return updatedState;
+}
+
+function compareScore (a, b) {
+  if (a['voteScore'] > b['voteScore']) {
+    return -1;
+  } else if (a['voteScore'] < b['voteScore']) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
 
 export default comments;
